a26ce7b476fcf375a3f1dd516c93e88e205e5191
Itoh Shimon <shimon@postfixnotation.org>
2019-07-20 02:00:24 +0900

    fix tests.

--- test.c
+++ test.c
@@ -5,7 +5,7 @@
 #include "thickslice_internal.h"
 
 #define assert_true(cond) do {					\
-		if (cond)					\
+		if (!(cond))					\
 			errx(1, "assert_true: %s", #cond);	\
 	} while (0)
 
@@ -21,9 +21,9 @@ void regress_vector(void)
 		*tmpi = i*i;
 		vector_push(vec, tmpi);
 	}
-	assert_true(*(int *)vector_get(vec, 0) == 1);
-	assert_true(*(int *)vector_get(vec, 4) == 25);
-	assert_true(*(int *)vector_get(vec, 9) == 100);
+	assert_true(*(int *)vector_get(vec, 0) == 100);
+	assert_true(*(int *)vector_get(vec, 5) == 25);
+	assert_true(*(int *)vector_get(vec, 9) == 1);
 	for (i = 0; i < nveclen; i++)
 		free(vector_get(vec, i));
 	vector_del(vec);
